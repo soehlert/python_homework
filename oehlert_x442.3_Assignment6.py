7dea16dda6aafad55a1a3659340070d4e281b77d
Sam Oehlert <sam.oehlert@gmail.com>
2018-08-23 20:00:57 -0500

    add homework and text files that go along with it

---
+++ oehlert_x442.3_Assignment6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Write a function that accepts the name of a file and returns a tuple containing the number of lines, words, and characters that are in the file. (Hint: Use the split function of the string module to help you count the words in the file.) 
+def file_facts(filenm):
+    lines = 0
+    words = 0
+    chars = 0
+    with open(filenm) as f: 
+        for line in lines
+    return (lines, words, chars)
+
+def merge_lists(*lists):
+
+
+nums = (1, 20, 300, 400)
+print(list(map())) 
+print(x + 8 for x in nums)
+
+wl = {}
+def wordlists(**wl):
+
+
+(Optional) Write a function that combines several dictionaries by creating a new dictionary with all the keys of the original ones. If a key appears in more than one input dictionary, the value corresponding to that key in the new dictionary should be a list containing all the values encountered in the input dictionaries that correspond to that key.
